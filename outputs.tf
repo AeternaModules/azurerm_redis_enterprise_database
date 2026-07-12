53c740a3d2df92f2a424fc2177131212779f60e0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:54 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,52 +1,47 @@
-output "redis_enterprise_databases" {
-  description = "All redis_enterprise_database resources"
-  value       = azurerm_redis_enterprise_database.redis_enterprise_databases
-  sensitive   = true
-}
 output "redis_enterprise_databases_client_protocol" {
-  description = "List of client_protocol values across all redis_enterprise_databases"
-  value       = [for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : v.client_protocol]
+  description = "Map of client_protocol values across all redis_enterprise_databases, keyed the same as var.redis_enterprise_databases"
+  value       = { for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : k => v.client_protocol }
 }
 output "redis_enterprise_databases_cluster_id" {
-  description = "List of cluster_id values across all redis_enterprise_databases"
-  value       = [for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : v.cluster_id]
+  description = "Map of cluster_id values across all redis_enterprise_databases, keyed the same as var.redis_enterprise_databases"
+  value       = { for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : k => v.cluster_id }
 }
 output "redis_enterprise_databases_clustering_policy" {
-  description = "List of clustering_policy values across all redis_enterprise_databases"
-  value       = [for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : v.clustering_policy]
+  description = "Map of clustering_policy values across all redis_enterprise_databases, keyed the same as var.redis_enterprise_databases"
+  value       = { for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : k => v.clustering_policy }
 }
 output "redis_enterprise_databases_eviction_policy" {
-  description = "List of eviction_policy values across all redis_enterprise_databases"
-  value       = [for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : v.eviction_policy]
+  description = "Map of eviction_policy values across all redis_enterprise_databases, keyed the same as var.redis_enterprise_databases"
+  value       = { for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : k => v.eviction_policy }
 }
 output "redis_enterprise_databases_linked_database_group_nickname" {
-  description = "List of linked_database_group_nickname values across all redis_enterprise_databases"
-  value       = [for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : v.linked_database_group_nickname]
+  description = "Map of linked_database_group_nickname values across all redis_enterprise_databases, keyed the same as var.redis_enterprise_databases"
+  value       = { for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : k => v.linked_database_group_nickname }
 }
 output "redis_enterprise_databases_linked_database_id" {
-  description = "List of linked_database_id values across all redis_enterprise_databases"
-  value       = [for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : v.linked_database_id]
+  description = "Map of linked_database_id values across all redis_enterprise_databases, keyed the same as var.redis_enterprise_databases"
+  value       = { for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : k => v.linked_database_id }
 }
 output "redis_enterprise_databases_module" {
-  description = "List of module values across all redis_enterprise_databases"
-  value       = [for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : v.module]
+  description = "Map of module values across all redis_enterprise_databases, keyed the same as var.redis_enterprise_databases"
+  value       = { for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : k => v.module }
 }
 output "redis_enterprise_databases_name" {
-  description = "List of name values across all redis_enterprise_databases"
-  value       = [for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : v.name]
+  description = "Map of name values across all redis_enterprise_databases, keyed the same as var.redis_enterprise_databases"
+  value       = { for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : k => v.name }
 }
 output "redis_enterprise_databases_port" {
-  description = "List of port values across all redis_enterprise_databases"
-  value       = [for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : v.port]
+  description = "Map of port values across all redis_enterprise_databases, keyed the same as var.redis_enterprise_databases"
+  value       = { for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : k => v.port }
 }
 output "redis_enterprise_databases_primary_access_key" {
-  description = "List of primary_access_key values across all redis_enterprise_databases"
-  value       = [for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : v.primary_access_key]
+  description = "Map of primary_access_key values across all redis_enterprise_databases, keyed the same as var.redis_enterprise_databases"
+  value       = { for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : k => v.primary_access_key }
   sensitive   = true
 }
 output "redis_enterprise_databases_secondary_access_key" {
-  description = "List of secondary_access_key values across all redis_enterprise_databases"
-  value       = [for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : v.secondary_access_key]
+  description = "Map of secondary_access_key values across all redis_enterprise_databases, keyed the same as var.redis_enterprise_databases"
+  value       = { for k, v in azurerm_redis_enterprise_database.redis_enterprise_databases : k => v.secondary_access_key }
   sensitive   = true
 }
 
